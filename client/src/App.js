f4eb09b09f5f6fa918ca8473d62818aeb4161723
KhalidRe <khaledtumah@gmail.com>
2025-01-26 13:37:03 +0100

    webvitals

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
+import reportWebVitals from "./reportWebVitals";
 
 const socket = io("http://localhost:5000");
 
